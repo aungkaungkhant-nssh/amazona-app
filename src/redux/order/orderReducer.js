f40865a047eea51ea19adc076dd0bd2d448ccedb
aungkaungkhant-nssh <akkgit0909@gmail.com>
2022-02-22 11:19:05 +0630

    implement order payment

--- src/redux/order/orderReducer.js
+++ src/redux/order/orderReducer.js
@@ -1,4 +1,4 @@
-import {ORDER_CREATE_REQUEST,ORDER_CREATE_SUCCESS,ORDER_CREATE_FAIL,ORDER_CREATE_RESET, ORDER_DETAIL_REQUEST, ORDER_DETAIL_SUCCESS, ORDER_DETAIL_FAIL} from './orderType'
+import {ORDER_CREATE_REQUEST,ORDER_CREATE_SUCCESS,ORDER_CREATE_FAIL,ORDER_CREATE_RESET, ORDER_DETAIL_REQUEST, ORDER_DETAIL_SUCCESS, ORDER_DETAIL_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_RESET} from './orderType'
 
 export const orderCreateReducer = (state = {},action) =>{
     switch(action.type){
@@ -23,5 +23,18 @@ export const orderDetailsReducer = (state ={loading:true,order:{}},action)=>{
         case ORDER_DETAIL_FAIL:
             return {loading:false,error:action.payload};
         default : return state;
+    }
+}
+export const orderPayReducer =(state = {loading:true,success:false},action)=>{
+    switch(action.type){
+        case ORDER_PAY_REQUEST:
+            return {loading:true};
+        case ORDER_PAY_SUCCESS:
+            return {loading:false,success:true};
+        case ORDER_PAY_FAIL:
+            return { loading: false, error: action.payload };
+        case ORDER_PAY_RESET:
+            return {};
+        default:return state;
     }
 }
