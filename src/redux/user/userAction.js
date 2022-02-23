44d5edca7d6b513053d2346417c45c5acdb6a8d2
aungkaungkhant-nssh <akkgit0909@gmail.com>
2022-02-23 11:02:58 +0630

    display user profile

--- src/redux/user/userAction.js
+++ src/redux/user/userAction.js
@@ -1,5 +1,5 @@
 import  Axios  from "axios";
-import { USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS,USER_SIGNIN_FAIL, USER_SIGNOUT, USER_SIGNUP_REQUEST, USER_SIGNUP_FAIL, USER_SIGNUP_SUCCESS } from "./userType"
+import { USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS,USER_SIGNIN_FAIL, USER_SIGNOUT, USER_SIGNUP_REQUEST, USER_SIGNUP_FAIL, USER_SIGNUP_SUCCESS, USER_DETAIL_REQUEST, USER_DETAIL_FAIL, USER_DETAIL_SUCCESS } from "./userType"
 
 export const signin = (email,password) => async(dispatch)=>{
     
@@ -34,11 +34,26 @@ export const signup =(name,email,password)=>async(dispatch)=>{
 }
 
 export const signout = ()=> async(dispatch,getState)=>{
-    
     localStorage.removeItem("cartItems");
     getState().cart.cartItems=[];
     localStorage.removeItem("userInfo");
     localStorage.removeItem("shippingAddress")
-    dispatch({type:USER_SIGNOUT});
-    
+    dispatch({type:USER_SIGNOUT});   
+}
+export const detailUser = ()=> async(dispatch,getState)=>{
+    dispatch({type:USER_DETAIL_REQUEST});
+    try{
+        let {userSignin:{userInfo}} = getState();
+        let {data} = await Axios.get('/api/users/details',{
+            headers:{
+                Authorization:`Bearer ${userInfo.token}`
+            }
+        })
+        dispatch({type:USER_DETAIL_SUCCESS,payload:data})
+    }catch(err){
+        dispatch({
+            type:USER_DETAIL_FAIL,
+            payload:err.response && err.response.data.message || err.message
+        });
+    }
 }
