cccd3b1f18b34a0122ca1693f6f14d297efba414
aungkaungkhant-nssh <akkgit0909@gmail.com>
2022-02-28 09:04:48 +0630

    deliver order

--- src/redux/order/orderAction.js
+++ src/redux/order/orderAction.js
@@ -1,6 +1,7 @@
 import  Axios  from "axios";
 import { CART_EMPTY } from "../cart/cartType";
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DELETE_FAIL, ORDER_DELETE_REQUEST, ORDER_DELETE_SUCCESS, ORDER_DETAIL_FAIL, ORDER_DETAIL_REQUEST, ORDER_DETAIL_SUCCESS, ORDER_HISTORY_FAIL, ORDER_HISTORY_REQUEST, ORDER_HISTORY_SUCCESS, ORDER_LIST_FAIL, ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS } from "./orderType"
+import { 
+    ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DELETE_FAIL, ORDER_DELETE_REQUEST, ORDER_DELETE_SUCCESS, ORDER_DETAIL_FAIL, ORDER_DETAIL_REQUEST, ORDER_DETAIL_SUCCESS, ORDER_HISTORY_FAIL, ORDER_HISTORY_REQUEST, ORDER_HISTORY_SUCCESS, ORDER_LIST_FAIL, ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS,ORDER_DELIVER_REQUEST,ORDER_DELIVER_SUCCESS,ORDER_DELIVER_FAIL } from "./orderType"
 
 export const createOrder = (order)=> async(dispatch,getState)=>{
   
@@ -110,6 +111,24 @@ export const deleteOrder = (order) => async(dispatch,getState)=>{
     }catch(err){
         dispatch({
             type:ORDER_DELETE_FAIL,
+            payload:err.response && err.response.data.message || err.message
+        })
+    }
+}
+
+export const deliverOrder= (orderId) => async(dispatch,getState)=>{
+    dispatch({type:ORDER_DELIVER_REQUEST});
+    try{
+        let {userSignin:{userInfo}} = getState();
+        let {data}= await Axios.put(`/api/orders/${orderId}/deliver`,{},{
+            headers:{
+                Authorization:`Bearer ${userInfo.token}`
+            }
+        })
+        dispatch({type:ORDER_DELIVER_SUCCESS,payload:data});
+    }catch(err){
+        dispatch({
+            type:ORDER_DELIVER_FAIL,
             payload:err.response && err.response.data.message || err.message
         })
     }
